245779bebaca5d6710c40202da2501b814cdc48c
PERSONALPROJECTS <sribashchandradas57@gmail.com>
2025-07-02 18:00:06 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import { DataProvider } from './contexts/DataContext';
 import { LingoProvider } from 'lingo.dev/react/client';
