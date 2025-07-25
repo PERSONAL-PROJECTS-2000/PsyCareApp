98b5112e344b94630c0934fade6c1f7611f12d8e
PERSONALPROJECTS <sribashchandradas57@gmail.com>
2025-07-25 17:09:02 +0530

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from 'tailwindcss'
 
-export default {
-  content: [],
+const config: Config = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 } satisfies Config
-
+export default config;
