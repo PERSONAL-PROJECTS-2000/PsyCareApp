467988745b74b84337b5716cd16287302c18a645
PERSONALPROJECTS <sribashchandradas57@gmail.com>
2025-07-26 07:00:14 +0530

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{ts,jsx,tsx}',
   ],
   theme: {
     extend: {
